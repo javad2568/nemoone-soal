75f1b3dc81bd538d9e6126e17adcfcbc964d6127
javad2568 <117682959+javad2568@users.noreply.github.com>
2022-12-01 10:32:45 -0800

    Update 20.cs
    ...

--- 20.cs
+++ 20.cs
@@ -10,6 +10,18 @@ namespace ConsoleApplication14
     {
         static void Main(string[] args)
         {
+             int n = 15;
+            Console.WriteLine(F(15));
+            Console.ReadKey();
+
+        }
+        private static int F(int n)
+        {
+            if (n == 1) ;
+            {
+                return 2;
+            }
+            return 2 * F(n - 1) + 1;
         }
     }
 }
